1bf3ece5d4b80d709114f01f838f7dee93fc9e07
Qori El-Hafizh <contact@qori.dev>
2022-06-05 22:07:59 +0700

    remove unused import

--- backend/services/album.py
+++ backend/services/album.py
@@ -1,9 +1,19 @@
-class IAlbumService:
-    def get_albums():
-        pass
+from sqlalchemy.orm import Session
+from models.core import Album
+from schemas.extensions import AlbumExtended as AlbumSchema
 
-    def get_albums_byid(id: str):
-        pass
 
-    def insert_album():
-        pass
+class AlbumService:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_albums(self):
+        return self.db.query(Album)
+
+    def get_albums_byid(self, id: str):
+        return self.db.query(Album).filter(Album.id == id).first()
+
+    def insert_album(self, album: AlbumSchema):
+        db_album = Album(id="", title=album.title, year=album.year)
+        self.db.add(db_album)
+        self.db.commit()
