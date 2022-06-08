c17e28e81fbe56cd4b347d46382fc5ae2d9f8375
Qori El-Hafizh <contact@qori.dev>
2022-06-08 13:50:34 +0700

    layout

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='flex flex-col h-screen'>
       <Nav></Nav>
-      <div className='mx-auto mt-8 flex-grow w-full md:w-8/12 lg:w-6/12 px-2'>
+      <div className='mx-auto mt-8 flex-grow w-full max-w-[800px]'>
         <Outlet></Outlet>
       </div>
       <Player />
